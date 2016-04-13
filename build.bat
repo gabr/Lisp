7378956907c58a1cbf235e52dfeed37a7ee8b0be
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2016-04-13 18:48:33 +0200

    Main loop and improvements in build script, gitignore was created as well

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+
+gcc -std=c99 src\main.c -o bin\a.exe -Wall -Wextra -Werror -fmax-errors=5
---
+++ src/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+// buffer for user input
+#define INPUT_SIZE 2048
+static char input[INPUT_SIZE];
+
+/*
+ * Prints version and exit informations
+ */
+void print_header()
+{
+  puts("Lisp Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+}
+
+/*
+ * Prints prompt
+ */
+void prompt()
+{
+  fputs("lispy> ", stdout);
+}
+
+/*
+ * Reads data from user to input variable
+ */
+void read()
+{
+  fgets(input, INPUT_SIZE, stdin);
+}
+
+int main()
+{
+  // Print header
+  print_header();
+
+  // Main never ending loop
+  while(1)
+  {
+    // Show prompt
+    prompt();
+
+    // Read input
+    read();
+
+    /* Echo input back to user */
+    printf("No you're a %s", input);
+  }
+
+  return 0;
+}
