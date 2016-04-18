419d410aa075136eff8ba441f8163e881af2f6b1
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2016-04-18 14:06:08 +0200

    Run after successful build

--- buildAndRun.bat
+++ buildAndRun.bat
@@ -1,3 +1,8 @@
 @echo off
 
-gcc -std=c99 src\main.c -o bin\a.exe -Wall -Wextra -Werror -fmax-errors=5
+gcc -std=c99 src\main.c -o bin\perl.exe -Wall -Wextra -Werror -fmax-errors=5
+
+if NOT ERRORLEVEL 1 (
+    echo.
+    .\bin\perl.exe
+)
