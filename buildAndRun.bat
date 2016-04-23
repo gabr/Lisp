150ea22c4cbeebd8494702a3f52e397e3c28c7c9
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2016-04-23 16:22:23 +0200

    Multiline build command

--- buildAndRun.bat
+++ buildAndRun.bat
@@ -1,6 +1,13 @@
 @echo off
 
-gcc -std=c99 src\main.c -o bin\perl.exe -Wall -Wextra -Werror -fmax-errors=5
+gcc  src\main.c     ^
+     src\mpc\mpc.c  ^
+    -std=c99        ^
+    -o bin\perl.exe ^
+    -Wall           ^
+    -Wextra         ^
+    -Werror         ^
+    -fmax-errors=5
 
 if NOT ERRORLEVEL 1 (
     echo.
